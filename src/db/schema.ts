9f128927671988ec926d04dccfc53a10c41326b8
cao-joey <24caojoey@gmail.com>
2026-04-02 22:25:52 -0400

    Project setup

---
+++ src/db/schema.ts
@@ -0,0 +1,11 @@
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+// EXAMPLE TABLE DEFINITION 
+// export const usersTable = pgTable("users", {
+//   id: integer().primaryKey().generatedAlwaysAsIdentity(),
+//   name: varchar({ length: 255 }).notNull(),
+//   age: integer().notNull(),
+//   email: varchar({ length: 255 }).notNull().unique(),
+// });
+
+
